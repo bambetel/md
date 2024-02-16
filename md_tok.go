8067131e81793c44ae33e118a905d491edb2459e
lukasz <lukasz.mat@gmail.com>
2024-02-16 17:08:36 +0100

    blockquote isolation

--- md_tok.go
+++ md_tok.go
@@ -7,21 +7,44 @@ import (
 	"github.com/bambetel/colo"
 )
 
-func MdTok(lines []string) {
-	for i, l := range lines {
-		t := BlockType(l)
-		if len(l) < 1 {
-			fmt.Println("↔")
+func MdTok(lines []string, pre string) {
+	fmt.Printf("MdTok() %d %q\n", len(lines), pre)
+	g := colo.Green.Fmt
+	r := colo.Red.Fmt
+	c := colo.Cyan.Fmt
+
+	for i := 0; i < len(lines); i++ {
+		l := lines[i]
+		fmt.Print(c(fmt.Sprintf("[%s:%d]", pre, i)))
+		// blank line
+		if len(l) < 1 || isBlankLine(l) {
+			fmt.Println(i, "↔")
 			continue
 		}
+
+		// consume a blockquote
 		if l[0] == '>' {
-			// TODO blockquote container handling
-			fmt.Print(colo.BriYellow.Fmt("BQ"))
+			j := i                                           // to be set as an element after list end
+			for ; j < len(lines) && len(lines[j]) > 0; j++ { // TODO safeguard len(lines) - 1 ???
+				if lines[j][0] != '>' {
+					break
+				}
+			}
+
+			block := make([]string, 0, 10)
+			for k := i; k < j; k++ {
+				block = append(block, lines[k][2:]) // TODO handling `>`, len=1
+			}
+			MdTok(block, pre+">>>>")
+			i = j - 1
+			continue
 		}
+
+		t := BlockType(l)
 		p := getLinePrefix(l)
-		fmt.Print(fmt.Sprintf("%3d", i), colo.Red.Fmt(fmt.Sprintf("%5s ", t)))
-		c := colo.Green
-		fmt.Print(c.Fmt(tr(p, map[rune]rune{' ': '_'})))
+		fmt.Print(pre)
+		fmt.Print(fmt.Sprintf("%3d", i), r(fmt.Sprintf("%5s ", t)))
+		fmt.Print(g(tr(p, map[rune]rune{' ': '_'})))
 		fmt.Println(l[len(p):])
 	}
 	return
@@ -55,4 +78,13 @@ func tr(s string, replace map[rune]rune) string {
 		}
 	}
 	return sb.String()
+}
+
+func isBlankLine(l string) bool {
+	for _, c := range l {
+		if c != ' ' && c != '\t' {
+			return false
+		}
+	}
+	return true
 }
