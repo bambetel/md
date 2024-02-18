cb48152f0695ea1e6971d26cb3a73ab1a21e6f5e
lukasz <lukasz.mat@gmail.com>
2024-02-18 14:55:32 +0100

    cleaned up

--- md_tok.go
+++ md_tok.go
@@ -4,68 +4,66 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
-
-	"github.com/bambetel/colo"
 )
 
 // Md tokenizer, parser functions
 // Assumes (at least leading, meaningful in Md) spacing normalized to space
 // characters using tabstop value.
 
+type mdLine struct {
+	Prefix string
+	Marker string
+	Text   string
+}
+
+func BlankLine() mdLine {
+	return mdLine{"", "", ""}
+}
+
 // For now, just playground
-func MdTok(lines []string, pre string) {
+func MdTok(lines []string, pre string) []mdLine {
 	fmt.Printf("MdTok() %d %q\n", len(lines), pre)
-	g := colo.Green.Fmt
-	r := colo.Red.Fmt
-	fPrefix := colo.NewTxFormat(colo.Green).Fmt
-	fType := colo.NewTxFormat(colo.BgBlue).Fmt
-	fCounter := func(i int) string { return colo.NewTxFormat(colo.BriBlack).Fmt(fmt.Sprintf("%3d", i)) }
-	sig := func() string { return r("*") }
+
+	out := make([]mdLine, 0, 16)
 
 	for i := 0; i < len(lines); i++ {
 		l := lines[i]
 
 		// blank line
 		if len(l) < 1 || isBlankLine(l) {
-			fmt.Printf("%s %s\n", fCounter(i), "↔")
+			out = append(out, BlankLine())
 			continue
 		}
 
-		// isolate a blockquote
-		if l[0] == '>' {
-			j := i // to be set as an element after list end
-			for ; j < len(lines); j++ {
-				if len(lines[j]) < 1 {
-					break
-				}
-				if lines[j][0] != '>' {
-					break
-				}
-			}
+		// // isolate a blockquote
+		// if l[0] == '>' {
+		// 	j := i // to be set as an element after list end
+		// 	for ; j < len(lines); j++ {
+		// 		if len(lines[j]) < 1 {
+		// 			break
+		// 		}
+		// 		if lines[j][0] != '>' {
+		// 			break
+		// 		}
+		// 	}
+		//
+		// 	block := make([]string, 0, 10)
+		// 	for k := i; k < j; k++ {
+		// 		block = append(block, lines[k][min(len(lines[k]), 2):])
+		// 	}
+		//
+		// 	MdTok(block, pre+">>>>")
+		// 	i = j - 1
+		// 	continue
+		// }
 
-			block := make([]string, 0, 10)
-			for k := i; k < j; k++ {
-				block = append(block, lines[k][min(len(lines[k]), 2):])
-			}
-
-			MdTok(block, pre+">>>>")
-			i = j - 1
-			continue
-		}
 		p := getLinePrefix(l)
 		l = l[len(p):]
-
 		mark, l := stripLineMark(l)
-		fmt.Printf("%s %s%s%s%s\n", fCounter(i), fPrefix(tr(p, map[rune]rune{' ': '_'})), fType(mark), l, sig())
-		continue
-		// todo func (line)->(marker, line)
-		t := BlockType(l)
-
-		fmt.Print(fmt.Sprintf("%3d", i), r(fmt.Sprintf("%5s ", t)))
-		fmt.Print(g(tr(p, map[rune]rune{' ': '_'})))
-		fmt.Println(l[len(p):])
+		item := mdLine{p, mark, l}
+		out = append(out, item)
 	}
-	return
+	return out
 }
 
 // only a block mark or also blockquote?
