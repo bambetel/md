30d0f8ab03b2237594ed2774ec1842021803a212
lukasz <lukasz.mat@gmail.com>
2024-02-18 14:11:56 +0100

    making tokenizer (line -> prefix, marker, text)

--- md_tok.go
+++ md_tok.go
@@ -2,28 +2,36 @@ package md
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/bambetel/colo"
 )
 
+// Md tokenizer, parser functions
+// Assumes (at least leading, meaningful in Md) spacing normalized to space
+// characters using tabstop value.
+
+// For now, just playground
 func MdTok(lines []string, pre string) {
 	fmt.Printf("MdTok() %d %q\n", len(lines), pre)
 	g := colo.Green.Fmt
 	r := colo.Red.Fmt
-	c := colo.Cyan.Fmt
+	fPrefix := colo.NewTxFormat(colo.Green).Fmt
+	fType := colo.NewTxFormat(colo.BgBlue).Fmt
+	fCounter := func(i int) string { return colo.NewTxFormat(colo.BriBlack).Fmt(fmt.Sprintf("%3d", i)) }
+	sig := func() string { return r("*") }
 
 	for i := 0; i < len(lines); i++ {
 		l := lines[i]
-		fmt.Print(c(fmt.Sprintf("[%s:%d]", pre, i)))
 
 		// blank line
 		if len(l) < 1 || isBlankLine(l) {
-			fmt.Println(i, "↔")
+			fmt.Printf("%s %s\n", fCounter(i), "↔")
 			continue
 		}
 
-		// consume a blockquote
+		// isolate a blockquote
 		if l[0] == '>' {
 			j := i // to be set as an element after list end
 			for ; j < len(lines); j++ {
@@ -44,43 +52,15 @@ func MdTok(lines []string, pre string) {
 			i = j - 1
 			continue
 		}
-
-		t := BlockType(l)
-		// consume a list OR same-level lists, then split by marker and blank lines
-		// list item are taken with the helper blank lines (?)
-		if strings.HasPrefix(t, "li") {
-			fmt.Println("LIST ITEM")
-			j := i
-			for ; j < len(lines); j++ {
-				tt := BlockType(lines[j])
-				// TODO abstracted consume hard-wrapped block (?)
-				if tt != t {
-					// TODO NOTE: blank line lookforward, takes neighbour black lines to the list
-					if tt != "p" {
-						break
-					}
-					if strings.HasPrefix(lines[j], " ") {
-						if isLi(strings.TrimSpace(lines[j])) != 0 {
-							fmt.Println("LI sublist candidate: ", tt, lines[j])
-						} else {
-							fmt.Println("LI sub candidate: ", tt, lines[j])
-						}
-					} else {
-						// child item
-						fmt.Println("ELSE: ", tt, lines[j])
-					}
-				} else {
-					fmt.Println("LI sibling: ", tt, lines[j])
-				}
-			}
-			block := lines[i:j]
-			fmt.Printf("@LIST %d %q\n", len(block), block)
-			i = j - 1
-			continue
-		}
-
 		p := getLinePrefix(l)
-		fmt.Print(pre)
+		l = l[len(p):]
+
+		mark, l := stripLineMark(l)
+		fmt.Printf("%s %s%s%s%s\n", fCounter(i), fPrefix(tr(p, map[rune]rune{' ': '_'})), fType(mark), l, sig())
+		continue
+		// todo func (line)->(marker, line)
+		t := BlockType(l)
+
 		fmt.Print(fmt.Sprintf("%3d", i), r(fmt.Sprintf("%5s ", t)))
 		fmt.Print(g(tr(p, map[rune]rune{' ': '_'})))
 		fmt.Println(l[len(p):])
@@ -88,6 +68,46 @@ func MdTok(lines []string, pre string) {
 	return
 }
 
+// only a block mark or also blockquote?
+// handling: HR, H1..6, LI 1., -, a., A., - [x],
+// (?): ~~~/```
+func stripLineMark(line string) (string, string) {
+	if len(line) < 2 {
+		return "", line
+	}
+	mark := ""
+	reH := regexp.MustCompile("^(#+)\\s+")
+	reLi := regexp.MustCompile("^(\\d+\\.|[a-zA-Z]\\.|[-+*]|[-+*]\\s+\\[[ x]\\])\\s+")
+	if isHR(line) {
+		return line, ""
+	} else if m := reH.FindString(line); len(m) > 0 {
+		mark = m
+	} else if m := reLi.FindString(line); len(m) > 0 {
+		mark = m
+	}
+	if len(mark) == len(line) {
+		// block type indicators (except for hr) require non-empty content
+		// otherwise it is just a line of text
+		return "", line
+	}
+	return line[:len(mark)], line[len(mark):]
+}
+
+func getLineMark(line string) string {
+	if isHR(line) {
+		return "---"
+	}
+	if isHeading(line) > 0 {
+		return "H"
+	}
+	if listType := isLi(line); listType != 0 {
+		return string(listType)
+	}
+	return ""
+}
+
+// Get Md line prefix that consists of whitespace and blockquote markers.
+// Meaning can be relative to the previous line.
 func getLinePrefix(l string) string {
 	i := 0
 	if len(l) < 1 {
